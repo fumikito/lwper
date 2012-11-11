0bf349102e28c0a10e830405a42080f2dc16ba92
Takahashi Fumiki <guy@hametuha.com>
2012-11-11 17:49:38 +0900

    adsense設置完了 close #2

--- functions/adsense.php
+++ functions/adsense.php
@@ -27,4 +27,53 @@ register_sidebar(array(
 ));
 add_action('init', '_lwper_adsense_sidebar');
 
-add_action();
+
+/**
+ * 投稿の上に広告を出す
+ * @param string $size laget, medium
+ */
+function lwper_ads($size = 'medium', $additional_class = ''){
+	$classes = array('lwp-ad');
+	if(wp_is_mobile()){
+		$classes[] = 'lwp-ad-mobile';
+	}
+	switch($size){
+		case 'large':
+			$classes[] = 'lwp-ad-large';
+			$ad = <<<EOS
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-0087037684083564";
+/* LWPERコンテンツ上 */
+google_ad_slot = "3467341910";
+google_ad_width = 336;
+google_ad_height = 280;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+EOS;
+			break;
+		default:
+			$classes[] = 'lwp-ad-medium';
+			$ad = <<<EOS
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-0087037684083564";
+/* LWPer投稿下 */
+google_ad_slot = "2636819148";
+google_ad_width = 300;
+google_ad_height = 250;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+EOS;
+			break;
+	}
+	if(!empty($additional_class)){
+		$classes[] = $additional_class;
+	}
+	printf('<div class="%s">%s</div>', implode(' ', $classes), $ad);
+}
+
