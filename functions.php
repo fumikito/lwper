2b14a1b660c3f46a18dbab4778c33609ea8086c0
fumikito <guy@hametuha.com>
2014-04-24 00:50:20 +0900

    Hook test.

--- functions.php
+++ functions.php
@@ -6,3 +6,4 @@ get_template_part('functions/adsense');
 get_template_part('functions/adminbar');
 get_template_part('functions/assets');
 get_template_part('functions/reference');
+get_template_part('functions/premium-themes');
